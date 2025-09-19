4f357bfb09326c9eaa1cb58f8ccf64be805e5e5f
Husnainarain2 <husnainabid9494@gmail.com>
2025-09-19 01:12:45 +0100

    3rd method is add in which we use for in loop

--- custom/custom react.js
+++ custom/custom react.js
@@ -24,14 +24,41 @@
 
 
 /****************** 2nd method ***************** */
-function customRender(mainconatiner){
-    const domElement=document.createElement('a');
-     const helement=document.createElement('h1');
-     helement.innerHTML=('hi i am husnain abid ');
-    domElement.innerHTML= 'click me to visit my email'
-    domElement.setAttribute('href','https://google.com');
-    domElement.setAttribute('target','_blank');
-    mainconatiner.append(domElement,helement);
+// function customRender(mainconatiner){
+//     const domElement=document.createElement('a');
+//      const helement=document.createElement('h1');
+//      helement.innerHTML=('hi i am husnain abid ');
+//     domElement.innerHTML= 'click me to visit my email'
+//     domElement.setAttribute('href','https://google.com');
+//     domElement.setAttribute('target','_blank');
+//     mainconatiner.append(domElement,helement);
+// }
+// const mainconatiner = document.querySelector('#root');
+// customRender(mainconatiner);
+
+/****************** 3rd method ***************** */
+function customRender(reactElement,element){
+    const domElement=document.createElement(reactElement.type);
+    domElement.innerHTML=reactElement.children;
+    for(const prop in reactElement.props){
+        if (prop === 'children') continue  // for in loop is used to excsee the properties of object 
+         {
+            domElement.setAttribute(prop,reactElement.props[prop])
+         
+        }
+           element.appendChild(domElement);
+    }
+
 }
-const mainconatiner = document.querySelector('#root');
-customRender(mainconatiner);
+const reactElement={
+    type:'a',
+    props:{
+        href:'https://google.com',
+        target:'_blank'
+    },
+    children:'click me to visit google'
+
+}
+const domElement = document.querySelector('#root');
+
+customRender(reactElement,domElement)
